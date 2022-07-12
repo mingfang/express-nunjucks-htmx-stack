ce58bc6c42a535a440685e85f3d747657195bea5
Ming Fang <mingfang@mac.com>
2022-07-12 21:22:21 +0000

    render from sub directories

--- server/index.js
+++ server/index.js
@@ -1,10 +1,35 @@
 const {server, listen} = require('./express.config')
 const {render} = require('./nunjucks/nunjucks.config')
 
-/* render template */
-server.get('/:template?', (req, res) => {
-  const template = req.params['template'] || 'index'
-  render(template, req, res)
+/* render *.njk files relative to templates dir */
+
+const renderPath = (subPath = '.') =>
+  (req, res) => {
+    const template = req.params['template'] || 'index'
+    render(`${subPath}/${template}`, req, res)
+  }
+
+server.get('/:template?', renderPath())
+
+/* render from sub directories */
+
+const fs = require('fs')
+const path = require('path')
+
+const walkDir = (dir, callback) => {
+  fs.readdirSync(dir).forEach(f => {
+    const dirPath = path.resolve(dir, f)
+    if (fs.statSync(dirPath).isDirectory() && !path.basename(dirPath).startsWith('_')) {
+      callback(dirPath)
+      walkDir(dirPath, callback)
+    }
+  })
+}
+
+const basePath = path.resolve(__dirname, '../templates')
+walkDir(basePath, filePath => {
+  const subPath = filePath.substring(basePath.length + 1)
+  server.get(`/${subPath}/:template?`, renderPath(subPath))
 })
 
 /* start */
