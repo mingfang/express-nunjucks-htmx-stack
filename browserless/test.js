18ee52b8dc8a9790b45c8fee8059fac83f4f7b78
Ming Fang <mingfang@mac.com>
2022-07-14 00:42:18 +0000

    add theme changer

--- browserless/test.js
+++
@@ -1,8 +0,0 @@
-const puppeteer = require('puppeteer-core')
-
-;(async ()=> {
-  const browser = await puppeteer.connect({browserWSEndpoint: 'ws://browserless.browserless-example:3000'})
-  const page = await browser.newPage()
-
-  await page.goto('http://httpbin.httpbin:5000')
-})()
--- browserless/test.py
+++
@@ -1,5 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-r = requests.get('http://httpbin.httpbin/headers')
-print(r.text)
